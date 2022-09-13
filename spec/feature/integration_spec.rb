860276850f29297c056ca3665d7efe0462b42669
unknown <ajzehri@gmail.com>
2022-09-12 22:36:40 -0500

    book attributes added

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -9,4 +9,25 @@ RSpec.describe 'Creating a book', type: :feature do
     visit books_path
     expect(page).to have_content('harry potter')
   end
+  scenario 'author inputs' do
+    visit new_book_path
+    fill_in 'Author', with: 'jk rowling'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('jk rowling')
+  end
+  scenario 'price inputs' do
+    visit new_book_path
+    fill_in 'Price', with: '10'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('10')
+  end
+  scenario 'publish date inputs' do
+    visit new_book_path
+    fill_in 'Publish Date', with: '4-23-2001'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('4-23-2001')
+  end
 end
